1b7f6dd7106a7b3f462546103bd9bb823fc4ad73
Bernardo Belchior <bernardo.belchior1@gmail.com>
2018-04-23 16:15:42 +0100

    Update web port

--- web/build/config.js
+++ web/build/config.js
@@ -7,7 +7,7 @@ module.exports = {
   // See https://webpack.js.org/configuration/dev-server
   devServer: {
     host: 'localhost',
-    port: 4000
+    port: 3000
   },
   // when you use electron please set to relative path like ./
   // otherwise only set to absolute path when you're using history mode
