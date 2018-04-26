4e30d90cd218fc7c0b3c9d86555e2539612bfbd8
Nuno Ramos <nunor07.unfc@gmail.com>
2018-04-26 18:32:24 +0100

    Starting solver gui

--- gui/src/renderer/components/LandingPage.vue
+++ gui/src/renderer/components/LandingPage.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="wrapper">
-    <el-button>Goods</el-button>
+    <el-card id="card-style">
+      <div slot="header" style="text-align: center;">
+        <h1>Ticket System</h1>
+      </div>
+      <el-row type="flex" justify="center">
+        <el-col :span="6">
+          <el-button>Sign-In</el-button>
+        </el-col>
+        <el-col :span="6">
+          <el-button>Sign-Up</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
@@ -18,4 +30,11 @@
   }
 </script>
 
-<style></style>
+<style scoped>
+
+#card-style {
+  max-width: 500px;
+  margin: auto;
+}
+
+</style>
